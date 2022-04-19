79e1cfab7956bc0a94b804d4cf524f59a054f98b
IamStarLord <mahaagro48@gmail.com>
2022-04-19 18:22:53 +0400

    First story and custom action

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,32 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Text, Dict, Any, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
 
+from rasa_sdk.executor import CollectingDispatcher
 
-# This is a simple example for a custom action which utters "Hello World!"
+import pandas as pd 
+import random 
+import csv
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionSortUser(Action):
+
+    def name(self) -> Text:
+        return "action_sort_user"
+
+    def run(self, 
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        houses = [("gryffindor", "where dwell the brave, courageous and chivalrous at heart"), 
+            ("ravenclaw", "who are know for their intelligence, wisdom and wit"), 
+            ("hufflepuff", "who value hardwork, dedication, patience and loyality"),
+            ("slytherin", "where roam the cunning, ambitious, resourceful people")]
+
+        # select a random house from the houses 
+        house = random.choice(houses)
+
+        # print the house to the dispatcher 
+        dispatcher.utter_message(text=f"I will sort you into {house[0]}, {house[1]}")
+
+        return []
