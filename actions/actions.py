5a00d55e20666a5ddd710adf5d82894ef64a5c1f
IamStarLord <mahaagro48@gmail.com>
2022-04-21 14:44:52 +0400

    Added new story cast a random spell

--- actions/actions.py
+++ actions/actions.py
@@ -8,6 +8,10 @@ import pandas as pd
 import random 
 import csv
 
+import random 
+import nltk
+from nltk.corpus import names 
+
 class ActionSortUser(Action):
 
     def name(self) -> Text:
@@ -29,4 +33,60 @@ class ActionSortUser(Action):
         # print the house to the dispatcher 
         dispatcher.utter_message(text=f"I will sort you into {house[0]}, {house[1]}")
 
+        return []
+
+class ActionCastSpell(Action):
+
+    def name(self) -> Text:
+        return "action_cast_spell"
+
+    def run(self, 
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker, 
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+            
+        # access the file 
+        spells = pd.read_csv("data/harry_potter/Spells.csv", sep=";")
+        spells = spells["Incantation"].to_list()
+        spells = set(spells)
+        spells = list(spells)
+        spells.remove("Unknown")
+        spells = [x for x in spells if pd.isnull(x) == False]
+        spell = random.choice(spells)
+
+        dispatcher.utter_message(text=spell)
+
+        return []
+
+class ActionGreetName(Action):
+
+    labeled_names = ([(name, "male") for name in names.words("male.txt")] + 
+                     [(name, "female") for name in names.words("female.txt")])
+
+    random.shuffle(labeled_names)
+    features = [({"last_letter": lambda n: n[-5:]}, gender) for (n, gender) in labeled_names]
+
+    train_set, test_set = features[:500], features[500:]
+    classifier = nltk.NaiveBayesClassifier.train(train_set)
+
+    def name(self) -> Text:
+        return "action_greet_name"
+
+    # # helper function 
+    # def gender_features(word):
+    #     return {"last_letter": word[-1]}
+    
+    def run(self,
+            dispatcher: CollectingDispatcher, 
+            tracker: Tracker, 
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]: 
+        # get the word from the entity 
+        for blob in tracker.latest_message["entities"]:
+            if blob["entity"] == "user_name":
+                name = blob["value"]
+                result = self.classifier.classify({"last_letter": name[-5:]})
+                if result == "female":
+                    dispatcher.utter_message(text=f"Hi {name}. Yer a witch, {name}")
+                elif result == "male":
+                    dispatcher.utter_message(text=f"Hi {name}. Yer a wizard, {name}")
         return []
